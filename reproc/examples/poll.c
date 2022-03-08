fe81200f4a2ab1bdfcb79f1c520410cd96a49a3b
Yonggang Luo <luoyonggang@gmail.com>
2022-03-08 23:54:50 +0800

    printf more

--- reproc/examples/poll.c
+++ reproc/examples/poll.c
@@ -140,7 +140,7 @@ static int child(void)
 #endif
   srand(((unsigned int) getpid()));
   for (i = 0; i < 100; ++i) {
-    ms = rand() % 20 + 10; // NOLINT
+    ms = rand() % 100 + 50; // NOLINT
     millisleep(ms);
     fprintf(stdout, "Process %i slept %i milliseconds stdout.\n", getpid(), ms);
     fprintf(stderr, "Process %i slept %i milliseconds stderr.\n", getpid(), ms);
